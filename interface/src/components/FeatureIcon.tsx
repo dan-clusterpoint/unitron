799a2af7aeb93fd255cfcda6eafa1b4d7cdeb2f1
dan-clusterpoint <dan@highpasscapital.com>
2025-07-19 16:31:09 -0700

    feat: allow FeatureIcon size overrides

--- interface/src/components/FeatureIcon.tsx
+++ interface/src/components/FeatureIcon.tsx
@@ -2,10 +2,13 @@ import type { ComponentType } from 'react'
 
 export interface FeatureIconProps {
   Icon: ComponentType<React.SVGProps<SVGSVGElement>>
+  className?: string
 }
 
-export default function FeatureIcon({ Icon }: FeatureIconProps) {
+export default function FeatureIcon({ Icon, className = '' }: FeatureIconProps) {
   return (
-    <Icon className="w-8 h-8 md:w-6 md:h-6 lg:w-5 lg:h-5 text-primary transform transition-transform lg:scale-90" />
+    <Icon
+      className={`w-8 h-8 md:w-6 md:h-6 lg:w-5 lg:h-5 text-primary transform transition-transform lg:scale-90 ${className}`}
+    />
   )
 }
