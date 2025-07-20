2ed8fcc8e7e8c6f98591d73338ff05f702aa0bc4
dan-clusterpoint <dan@highpasscapital.com>
2025-07-19 17:40:38 -0700

    refactor: use class for feature icon sizing

--- interface/src/components/FeatureIcon.tsx
+++ interface/src/components/FeatureIcon.tsx
@@ -1,4 +1,5 @@
 import type { ComponentType } from 'react'
+import clsx from 'clsx'
 
 export interface FeatureIconProps {
   Icon: ComponentType<React.SVGProps<SVGSVGElement>>
@@ -8,7 +9,10 @@ export interface FeatureIconProps {
 export default function FeatureIcon({ Icon, className = '' }: FeatureIconProps) {
   return (
     <Icon
-      className={`w-8 h-8 md:w-6 md:h-6 lg:w-5 lg:h-5 text-primary transform transition-transform lg:scale-90 ${className}`}
+      className={clsx(
+        'feature-icon text-primary transform transition-transform lg:scale-90',
+        className
+      )}
     />
   )
 }
