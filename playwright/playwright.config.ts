7ddcdb2f9afcb544c9226892d37570d58132fdc4
dan-clusterpoint <dan@highpasscapital.com>
2025-07-30 12:18:06 -0700

    Fix Playwright test workflow

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: '.',
+  use: {
+    baseURL: 'http://localhost:5173'
+  },
   webServer: {
     command: 'npm run dev -- --port 5173',
     url: 'http://localhost:5173',
